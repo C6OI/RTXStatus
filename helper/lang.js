1ce3e6358c8cf3ee9702312086de593c531a768b
C6OI <C6OI@yandex.ru>
2021-11-09 13:20:35 +0500

    Removing merge conflicts

--- helper/lang.js
+++ helper/lang.js
@@ -1,7 +1,6 @@
 // Authored by David Loznukho (Keneshin)
 module.exports = {
     en: {
-<<<<<<< HEAD
         connectFailed: "Failed to connect",
         overcrowded: "Server is overcrowded.\nWait for space to become free.",
         connected: "Connected",
@@ -33,38 +32,5 @@ module.exports = {
         waiting: ":flag_us: Waiting for the initial packet...\n\n:flag_ru: Ожидание начального пакета...",
         maintenance: ":flag_us: Maintenance\n\n:flag_ru: Технические работы",
         serverIsOnMaintenance: ":flag_us: Server is on maintenance.\nPlease wait while maintenance is over.\n\n:flag_ru: Сейчас проводятся тех. работы.\nПожалуйста, подождите, пока тех. работы не будут окончены."
-=======
-        connectfailed: "Failed to connect",
-        overcrowded: "Server is overcrowded.\nWait for space to become free.",
-        connected: "Connected!",
-        serverisonline: "Server is online, you can play now.",
-        pleasewait: "Please wait!",
-        connecting: "Connecting to the server...",
-        waiting: "Waiting for the initial packet...",
-        maintenance: "Maintenance",
-        serverisonmaintenance: "Server is on maintenance. Please wait while maintenance is over."
-     },
-     ru: {
-        connectfailed: "Не удалось подключиться",
-        overcrowded: "Сервер переполнен. Пожалуйста, подождите, пока освободится место!",
-        connected: "Подключено!",
-        serverisonline: "Сервер онлайн, вы можете начать играть.",
-        pleasewait: "Пожалуйста подождите!",
-        connecting: "Подключаемся к серверу...",
-        waiting: "Ожидание начального пакета...",
-        maintenance: "Тех. работы",
-        serverisonmaintenance: "Сейчас проводятся тех. работы. Пожалуйста, подождите, пока тех. работы не будут окончены."
-     },
-     all: {
-        connectfailed: ":flag_us: Failed to connect\n:flag_ru: Не удалось подключиться",
-        overcrowded: ":flag_us: Server is overcrowded.\nWait for space to become free.\n\n:flag_ru: Сервер переполнен.\nДождитесь, пока освободится место.",
-        connected: ":flag_us: Connected\n\n:flag_ru: Подключено",
-        serverisonline: ":flag_us: Server is online.\nYou can play now!\n\n:flag_ru: Сервер в сети.\nВы можете играть!",
-        pleasewait: ":flag_us: Please, wait\n\n:flag_ru: Пожалуйста, подождите",
-        connecting: ":flag_us: Connecting to server...\n\n:flag_ru: Подключение к серверу...",
-        waiting: ":flag_us: Waiting for initial packet...\n\n:flag_ru: Ожидание начального пакета...",
-        maintenance: ":flag_us: Maintenance\n\n:flag_ru: Технические работы",
-        serverisonmaintenance: ":flag_us: Server on maintenance.\nWait for the server to start.\n\n:flag_ru: Сервер на тех. обслуживании.\nПодождите, пока его снова запустят."
->>>>>>> 2dbdb3b66e04c37a305e2a23377bb1e071435694
      }
 }
